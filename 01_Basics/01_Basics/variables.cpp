2b1bc381ce2d82d85abcc9195e61909e11edba3f
Jawad Afzal <jawad01afzal@gmail.com>
2026-06-24 09:58:41 +0500

    Create variables.cpp

---
+++ 01_Basics/01_Basics/variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int age = 19;
+    char grade = 'A';
+    cout << "Age: " << age << endl;
+    cout << "Grade: " << grade;
+    return 0;
+}
+
